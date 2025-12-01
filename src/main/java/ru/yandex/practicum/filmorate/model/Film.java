6f9c81cacc68d13124a8603b02dc556f6f67e81c
Nikita Ukhov <exclave.exe@yandex.ru>
2025-12-01 16:55:32 +0500

    fix user/film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,17 +1,34 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.validation.ReleaseDateValid;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 @Data
 public class Film {
+    @NotNull
     private Long id;
+
+    @NotBlank(message = "Name не может быть пустым")
     private String name;
+
+    @Size(max = 200, message = "Описание не должно превышать 200 символов")
     private String description;
+
+    @ReleaseDateValid // Собственная аннотация
     private LocalDate releaseDate;
+
+    @Positive
     private Integer duration;
+
+    @NotNull
     private Mpa mpa;
+
     private Set<Genre> genres;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class User {
+    @NotNull
     private Long id;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Email должен содержать символ: @")
     private String email;
+
+    @NotBlank(message = "Login не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не должен содержать пробелы")
     private String login;
+
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
+
     private String name;
 }
